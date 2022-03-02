9a3bb98404687b86312bd48418b144df5c807ebc
Doug Cameron <dcameron@brightree.com>
2022-03-02 11:32:09 +0000

    migrating to statiq

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+
+await Bootstrapper
+        .Factory
+        .CreateWeb(args)
+        .DeployToGitHubPages(
+                "Nyami",
+                "nyami.github.io",
+                Config.FromSetting<string>("GITHUB_TOKEN"))
+        .RunAsync();
